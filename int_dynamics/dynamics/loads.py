Dynamics system completely working. One little bug in mass calculation left to squash.

int_dynamics/dynamics/loads.py
@@ -25,7 +25,7 @@ class OneDimensionalLoad:
             "inverted": inverted
         })
 
-    def get_state_derivatives(self, mass):
+    def get_state_derivatives(self):
         state_derivatives = {
             self.position: self.velocity,
             self.velocity: 0
@@ -33,24 +33,23 @@ class OneDimensionalLoad:
         for wheel in self.wheels:
             caster = -1 if wheel["inverted"] else 1
             wheel["wheel"].velocity = np.array([0, 1]) * self.velocity * caster
-            state_derivatives.update(wheel["wheel"].get_state_derivatives(mass))
+            state_derivatives.update(wheel["wheel"].get_state_derivatives(self.mass))
             state_derivatives[self.velocity] += state_derivatives[wheel["wheel"].velocity][1] * caster
         return state_derivatives
 
 
-class TwoDimensionalLoad(OneDimensionalLoad):
+class TwoDimensionalLoad:
     """
     Simulates the dynamics of a load that can move in two dimensions and rotate in its plane provided any number of motive forces.
     """
 
     def __init__(self, mass):
-        super().__init__(None, mass)
-        self.state = {
-            "position": theano.shared(np.array([0.0, 0.0, 0.0]), theano.config.floatX),
-            "velocity": theano.shared(np.array([0.0, 0.0, 0.0]), theano.config.floatX)
-        }
+        self.mass = mass
+        self.velocity = theano.shared(np.array([0.0, 0.0, 0.0]), theano.config.floatX)
+        self.position = theano.shared(np.array([0.0, 0.0, 0.0]), theano.config.floatX)
+        self.wheels = []
 
-    def add_input(self, component, x_origin=0, y_origin=0, r_origin=0):
+    def add_wheel(self, component, x_origin=0, y_origin=0, r_origin=0):
         """
         Adds a motive force to the load located at (x_origin, y_origin) away from the cog and rotated r_origin radians
         from forward-facing
@@ -59,67 +58,35 @@ class TwoDimensionalLoad(OneDimensionalLoad):
         :param y_origin: The y distance, in feet, from the load's center of gravity
         :param r_origin: The angle, in radians, to apply the force at
         """
-        self.input_components.append({
-            "component": component,
+        self.wheels.append({
+            "wheel": component,
             "origin": [x_origin, y_origin, r_origin],
             "distance_to_cog": math.sqrt(x_origin**2 + y_origin**2),
             "angle_to_perpendicular": math.pi-math.atan2(y_origin, x_origin)-r_origin
         })
 
-    def get_force_tensor(self):
-        bot_to_world_matrix = rot_matrix(self.state["position"][2])
-        robot_force = self.get_input_force_tensor()
-        return T.dot(robot_force, bot_to_world_matrix)
+    def get_state_derivatives(self):
+        state_derivatives = {self.position: self.velocity}
 
-    def get_input_force_tensor(self):
-        input_force = [0.0, 0.0, 0.0]
-        for component in self.input_components:
-            origin_to_bot_rot = rot_matrix(component["origin"][2])
-            origin_to_bot_rot += np.array([[0,0,1],[0,0,0],[0,0,0]]) * math.sin(component["angle_to_perpendicular"])/component["distance_to_cog"]
-            origin_to_bot_rot += np.array([[0,0,0],[0,0,1],[0,0,0]]) * math.cos(component["angle_to_perpendicular"])/component["distance_to_cog"]
+        bot_to_world = rot_matrix(self.position[2])
+        world_to_bot = rot_matrix(-self.position[2])
 
-            force = component["component"].get_force_tensor()
-            force = T.concatenate((force, np.array([0])))
-            input_force += T.dot(force, origin_to_bot_rot)
-        return input_force
+        robot_velocity = T.dot(self.velocity, world_to_bot)
+        robot_acceleration = []
 
-    def build_source_state_derivatives(self, travel, velocity, dt):
-        world_to_robot_rot = rot_matrix(-self.state_derivatives["position"][2] + travel[2] / 2)
-        bot_travel = T.dot(travel, world_to_robot_rot)
-        bot_velocity = T.dot(velocity, world_to_robot_rot)
+        for wheel in self.wheels:
 
-        for component in self.input_components:
-            bot_to_origin_rot = rot_matrix(-component["origin"][2])
-            bot_to_origin_rot += np.array([[0,0,0],[0,0,0],[1,0,0]]) * math.sin(-component["angle_to_perpendicular"])/component["distance_to_cog"]
-            bot_to_origin_rot += np.array([[0,0,0],[0,0,0],[0,1,0]]) * math.cos(-component["angle_to_perpendicular"])/component["distance_to_cog"]
+            bot_to_wheel = rot_matrix(-wheel["origin"][2])[:, 0:2]
+            bot_to_wheel += np.array([[0,0],[0,0],[1,0]]) * math.sin(-wheel["angle_to_perpendicular"])/wheel["distance_to_cog"]
+            bot_to_wheel += np.array([[0,0],[0,0],[0,1]]) * math.cos(-wheel["angle_to_perpendicular"])/wheel["distance_to_cog"]
 
-            component["component"].build_state_updates(
-                T.dot(bot_travel, bot_to_origin_rot)[:2],
-                T.dot(bot_velocity, bot_to_origin_rot)[:2],
-                dt)
+            wheel_to_bot = rot_matrix(wheel["origin"][2])[0:2]
+            wheel_to_bot += np.array([[0,0,1],[0,0,0]]) * math.sin(wheel["angle_to_perpendicular"])/wheel["distance_to_cog"]
+            wheel_to_bot += np.array([[0,0,0],[0,0,1]]) * math.cos(wheel["angle_to_perpendicular"])/wheel["distance_to_cog"]
 
-    def build_integration(self, dt):
-        fake_travel = self.state["velocity"]*dt
-        self.build_state_updates(fake_travel, self.state["velocity"])
-        force = self.get_force_tensor()
-        acceleration = force/self.mass
-
-        acceleration_derivative = theano.gradient.jacobian(acceleration, self.state["velocity"])
-
-        # Taylor series integral
-        def series_advance(i, last_term, acc_deriv, dt):
-            next_term = T.dot(acc_deriv, last_term)*dt/i
-            return next_term, theano.scan_module.until(T.all(abs(next_term) < 10e-7))
-
-        init_term = dt*T.identity_like(acceleration_derivative)
-
-        terms, _ = theano.scan(series_advance,
-                               sequences=[T.arange(2, 500)],
-                               non_sequences=[acceleration_derivative, dt],
-                               outputs_info=init_term,
-                               )
-        new_deltav = T.dot(T.sum(terms, axis=0) + init_term, acceleration)
-
-        new_velocity = self.state["velocity"] + new_deltav
-        real_travel = (self.state["velocity"] + new_velocity)*dt/2
-        self.build_state_updates(real_travel, new_velocity)
+            wheel["wheel"].velocity = T.dot(robot_velocity, bot_to_wheel)
+            state_derivatives.update(wheel["wheel"].get_state_derivatives(self.mass))
+            robot_acceleration.append(T.dot(state_derivatives[wheel["wheel"].velocity], wheel_to_bot))
+        total_acc = T.sum(robot_acceleration, axis=0)
+        state_derivatives[self.velocity] = T.dot(total_acc, bot_to_world)
+        return state_derivatives

int_dynamics/dynamics/wheels.py
@@ -2,9 +2,6 @@ import math
 import numpy as np
 import theano
 import theano.tensor as T
-from theano.tensor import slinalg
-from theano.ifelse import ifelse
-from .utilities import integrate_via_ode
 
 
 class SimpleWheels:
@@ -39,25 +36,29 @@ class SolidWheels:
         self.total_dynamic_cof = normal_force*dynamic_cof
         # Ground velocity
         self.velocity = theano.shared(np.array([0.0, 0.0]), theano.config.floatX)
-        # Difference between wheel surface velocity and ground velocity
-        self.slip = theano.shared(np.array([0.0, 0.0]), theano.config.floatX)
 
     def get_state_derivatives(self, load_mass):
+        # Difference between wheel surface velocity and ground velocity
+        slip = theano.shared(0.0, theano.config.floatX)
+
         circumference = (math.pi * self.diameter)
-        self.gearbox.velocity = (self.velocity[1] + self.slip[1])/circumference
+        self.gearbox.velocity = (self.velocity[1] + slip)/circumference
         state_derivatives = self.gearbox.get_state_derivatives(load_mass)
 
         force_in = state_derivatives[self.gearbox.velocity]*circumference*load_mass
 
-        force_out = T.clip(force_in, -self.total_static_cof, self.total_static_cof) + self.slip
-        state_derivatives[self.slip] = (force_in - force_out)/self.mass
+        force_out = np.array([0, 1]) * T.clip(force_in, -self.total_static_cof, self.total_static_cof) + \
+                    T.clip(slip, -self.total_dynamic_cof, self.total_dynamic_cof)
+        force_out += np.array([1, 0]) * T.clip(-self.velocity[0], -self.total_dynamic_cof, self.total_dynamic_cof)
+        state_derivatives[slip] = (force_in - force_out[1])/self.mass
         state_derivatives[self.velocity] = force_out/self.mass
         return state_derivatives
 
 
 class KOPWheels(SolidWheels):
     def __init__(self, gearbox, diameter, count, normal_force):
-        SolidWheels.__init__(self, gearbox, count, diameter, .00107, .9, normal_force)
+        SolidWheels.__init__(self, gearbox, diameter, count, 1.07, .9, normal_force)
+
 
 class MecanumWheel:
     """